60fdee9a5133797d77411431cee8cbf0328e1cf6
Faizan Gillani <165652722+faizan-gp@users.noreply.github.com>
2026-01-21 23:49:52 +0800

    Pixel integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientShell from "./components/ClientShell";
@@ -74,6 +75,34 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
+        <Script
+          id="meta-signals-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !(function(f,b,e,v,vv,n,nn,t,s,tt,ss){
+                if (!f.cbq){nn = f.cbq = function(){nn.initialized ? nn.apply(f.cbq, arguments) : nn.queue.push(arguments);};
+                if(!f._cbq) f._cbq = nn;
+                nn.push = nn; nn.loaded = !0; nn.version = '2.0'; nn.queue = [];
+                tt = b.createElement(e); tt.async = !0; tt.src = vv; ss=b.getElementsByTagName(e)[0]; ss.parentNode.insertBefore(tt,ss);}
+                if (f.xbq) return; if (f.fbq) f.xbq=f.fbq;
+                n = f.fbq = function()
+                {if(arguments[0].startsWith('track')){n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);f.cbq.initialized ? f.cbq.apply(f.cbq, arguments) : f.cbq.queue.push(arguments);}
+                else{n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);}};
+                if(!f._fbq) f._fbq = n;
+                n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = (f.xbq)?f.xbq.queue:[];
+                t = b.createElement(e); t.async = !0; t.src = v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s);
+              })
+              (window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js', 'https://tracking.printbrawl.com/sdk/2144149738840324497/events.js');
+              fbq('init', '1420495289671184');
+              fbq('track', 'PageView');
+              cbq('setHost', 'https://tracking.printbrawl.com');
+              cbq('init', '2144149738840324497');
+              cbq('set', 'integrationMethod', 'forkFromSnippetCode');
+              cbq('track', 'PageView');
+            `,
+          }}
+        />
         <OrganizationSchema />
         <WebSiteSchema />
         <SiteNavigationSchema />
